55e5d1a4b6050fa846b5c448d506efeab1e11818
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-07-11 17:08:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,9 +9,15 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
+// The greet function should take one argument, a String which specifies the 24-hour time in the format HH:MM.
+// If the time is earlier than 12pm, return "Good Morning".
+// If the time is between 12pm and 5pm, return "Good Afternoon".
+// If the time is later than 5pm, return "Good Evening".
+
 /* Write your implementation of greet() */
 function greet(timeString) {
   const time = parseInt(timeString.split(":")[0])
+<<<<<<< HEAD
 
   if (time < 12) {
     return "Good Morning";
@@ -20,6 +26,18 @@ function greet(timeString) {
   } else if (time >= 17){
     return "Good Evening";
   }
+=======
+  let message;
+
+  if (time < 12) {
+    message = "Good Morning";
+  } else if (time >= 12 && time <= 5){
+    message = "Good Afternoon";
+  } else if (time > 5){
+    message = "Good Evening";
+  }
+  return message
+>>>>>>> 91d2124fd7f70cb5e32af7c09c269cfd63a9be68
 }
 
 /* Write your implementation of displayMessage() */
